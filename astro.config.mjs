0f360244ef52fb7f64f65114cff2328b3aaf2829
Andrés Ignacio Maldonado <hello.a.maldonado@gmail.com>
2025-09-19 11:53:30 -0300

    feature: prefetch files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import prefetch from "@astrojs/prefetch";
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch()],
   output: 'static',
   site: 'https://andres-maldonado.com',
   vite: {
