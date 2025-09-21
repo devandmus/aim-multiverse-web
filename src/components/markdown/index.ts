5390c7c1e946c65ce8933a4f03b5ea414fc4bbb9
Andrés Ignacio Maldonado <hello.a.maldonado@gmail.com>
2025-09-21 12:03:18 -0300

    feature: clean unused components

--- src/components/markdown/index.ts
+++ src/components/markdown/index.ts
@@ -3,16 +3,3 @@ export { default as ImageCarousel } from './ImageCarousel.astro';
 export { default as VideoEmbed } from './VideoEmbed.astro';
 export { default as CodeBlock } from './CodeBlock.astro';
 export { default as Callout } from './Callout.astro';
-export { default as ResourceDownload } from './ResourceDownload.astro';
-
-// Componentes personalizados con clases Tailwind
-export { default as CustomHeading } from './CustomHeading.astro';
-export { default as CustomParagraph } from './CustomParagraph.astro';
-export { default as CustomList } from './CustomList.astro';
-export { default as CustomCode } from './CustomCode.astro';
-export { default as CustomPre } from './CustomPre.astro';
-export { default as CustomBlockquote } from './CustomBlockquote.astro';
-export { default as CustomLink } from './CustomLink.astro';
-export { default as CustomTable } from './CustomTable.astro';
-export { default as CustomImage } from './CustomImage.astro';
-export { default as CustomHR } from './CustomHR.astro';
